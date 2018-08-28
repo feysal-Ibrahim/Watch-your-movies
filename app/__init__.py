e0fda286ab1ea7b781071d912cdb805c1664dfb8
billowbashir <billowbashir@gmail.com>
2018-08-28 19:30:59 +0300

    get api response and use jinja2 macros

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from .config import DevConfig
 # initialising application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 # view
 @app.route('/')
 def index():
@@ -7,12 +8,21 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message='hello flask'
-    return render_template('index.html',message=message)
-    
+
+
+       # Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular_movies = popular_movies,upcoming=upcoming_movie,now_showing=now_showing_movie)
+
+
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id=movie_id)
+    title = 'movie - title is working'
+    return render_template('movie.html',id=movie_id, title=title)
