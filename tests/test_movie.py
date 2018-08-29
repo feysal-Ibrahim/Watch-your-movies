91299d8c5d5ee563b68f0df25c59a59e35098d0a
billowbashir <billowbashir@gmail.com>
2018-08-29 19:46:46 +0300

    fix image breaking error and run succesfully also changed folder structure

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,5 +1,5 @@
 import unittest
-from models import movie
+from app.models import Movie
 Movie = movie.Movie
 
 class MovieTest(unittest.TestCase):
@@ -22,6 +22,6 @@ class MovieTest(unittest.TestCase):
         self.assertEqual(self.new_movie.poster,'https://image.tmdb.org/t/p/w500/khsjha27hbs')
         self.assertEqual(self.new_movie.vote_average,8.5)
         self.assertEqual(self.new_movie.vote_count,129993)
-
-if __name__ == '__main__':
-    unittest.main()
+#
+# if __name__ == '__main__':
+#     unittest.main()
